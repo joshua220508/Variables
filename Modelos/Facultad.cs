bba2ac5e9625e91bc710bd7749596f4a12f85d71
joshua220508 <jesaenz@uamv.edu.ni>
2025-09-22 07:30:06 -0600

    Se creo la clase Facultad satisfactoriamente

---
+++ Modelos/Facultad.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Variables.Modelos
+{
+    public class Facultad
+    {
+        public String Nombre;
+        public String Codigo;
+
+        public String MostrarDatos()
+        {
+            return "Facultad: " + Nombre + " Codigo: " + Codigo;
+        }
+
+    }
+}
